29d0c2573ad14732331c0f2e109212003850ec1d
vuong200514 <106741622+vuong200514@users.noreply.github.com>
2025-01-06 20:40:26 +0700

    Update jji.lua

--- jji.lua
+++ jji.lua
@@ -2,27 +2,46 @@ repeat task.wait() until game:IsLoaded()
 
 if not getgenv().StingrayLoaded then
     getgenv().StingrayLoaded = true
-    
+
     -- Init --
     local StartTime = tick()
     local LocalPlayer = game:GetService("Players").LocalPlayer
-    
+
     -- Load Configs --
     -- Webhook
-    getgenv().Webhook = "https://discord.com/api/webhooks/899205719616811038/Eb9b3dGd_g8VweEAHPNogy1-OGvwcL8e1yutbw5z57VYm-o6bmLAsJueYAk7qcPV9T--"
-    
+    pcall(function()
+        if getgenv().Webhook then
+            writefile("JJI_Webhook.txt", getgenv().Webhook)
+        end
+        if readfile("JJI_Webhook.txt") then
+            getgenv().Webhook = readfile("JJI_Webhook.txt")
+        end
+    end)
+
+    -- -- Instant Kill
+    -- pcall(function()
+    --     if getgenv().InstantKill then
+    --         writefile("JJI_InstantKill.txt", getgenv().InstantKill)
+    --     end
+    --     if isfile("JJI_InstantKill.txt") then
+    --         getgenv().InstantKill = readfile("JJI_InstantKill.txt")
+    --     else
+    --         getgenv().InstantKill = "OFF"
+    --     end
+    -- end)
+
     -- Luck Boosts
     getgenv().LuckBoosts = {"Withered Beckoning Cat", "White Lotus", "Luck Vial", "Fortune Gourd"}
-    
     -- UI --
+    
     local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/vuong200514/VuongHub/refs/heads/main/ui.lua"))()
     local MainUI = UI.InitUI()
     local Toggle = "ON"
     pcall(function()
-        if isfile("Stingray/JJI_State.txt") then
-            Toggle = readfile("Stingray/JJI_State.txt")
+        if isfile("JJI_State.txt") then
+            Toggle = readfile("JJI_State.txt")
         else
-            writefile("Stingray/JJI_State.txt", "ON")
+            writefile("JJI_State.txt", "ON")
         end
     end)
     print("QUEUE TOGGLE: "..Toggle)
@@ -37,44 +56,39 @@ if not getgenv().StingrayLoaded then
         else
             Toggle = "OFF"
         end
-        writefile("Stingray/JJI_State.txt", Toggle)
-        print(readfile("Stingray/JJI_State.txt"))
+        writefile("JJI_State.txt", Toggle)
+        print(readfile("JJI_State.txt"))
     end)
-    
+
     -- Constants
     local Cats = {"Withered Beckoning Cat", "Wooden Beckoning Cat", "Polished Beckoning Cat"}
-    local Loti = {"White Lotus", "Sapphire Lotus", "Jade Lotus", "Iridescent Lotus"} -- Did you know the plural of Lotus is Loti
-    local Highlight = {"5 Demon Fingers", "Maximum Scroll", "Domain Shard", "Polished Beckoning Cat", "Demon Finger", "Energy Nature Scroll", "Purified Curse Hand"}
-    local Runners = {"Soul Curse"} -- Damn monkeys go AEEHOIIII, add to this table if you feel like a boss is running too much
-    
-    local QueueSuccess = "False"
+    local Loti = {"White Lotus", "Sapphire Lotus", "Jade Lotus", "Iridescent Lotus"}
+    local Highlight = {
+        "Maximum Scroll", "Domain Shard", "Demon Finger", "Energy Nature Scroll", "Purified Curse Hand"
+    }
+
     if Toggle == "ON" then
-        local Queued, QueueFail = pcall(function()
-            queue_on_teleport('loadstring(game:HttpGet("https://github.com/vuong200514/VuongHub/blob/main/jji.lua"))()')()
+        local s, e = pcall(function()
+            queue_on_teleport(getgenv().Script)()
         end)
-        if not Queued then
-            QueueSuccess = QueueFail
-        else
-            QueueSuccess = "True"
-        end
     end
-    
-    -- Black screen check & Fail safe --
+
+    -- Black screen check & Players in lobby check --
     task.spawn(function()
         task.wait(90)
         while true do
             game:GetService("TeleportService"):Teleport(10450270085)
-            task.wait(10)
+            task.wait(7)
         end
     end)
-    
+
     if game.PlaceId == 10450270085 then
         game:GetService("TeleportService"):Teleport(119359147980471)
     elseif game.PlaceId == 119359147980471 then
         local SelectedBoss = "Soul Curse"
         pcall(function()
-            if readfile("Stingray/JJI_LastBoss.txt") then
-                SelectedBoss = readfile("Stingray/JJI_LastBoss.txt")
+            if readfile("JJI_LastBoss.txt") then
+                SelectedBoss = readfile("JJI_LastBoss.txt")
             end
         end)
         task.wait(3)
@@ -82,9 +96,11 @@ if not getgenv().StingrayLoaded then
             game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Raids"):WaitForChild("QuickStart"):InvokeServer("Boss", SelectedBoss, "Nightmare")
         end
     end
-    
+
     repeat task.wait() until LocalPlayer.Character
     local Root = LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+    if not s then print("Better put this in auto execute, your thing doesn't support queue on tp:", e) end
+
     local Objects = workspace:WaitForChild("Objects")
     local Mobs = Objects:WaitForChild("Mobs")
     local Spawns = Objects:WaitForChild("Spawns")
@@ -94,7 +110,7 @@ if not getgenv().StingrayLoaded then
     local LootUI = LocalPlayer.PlayerGui:WaitForChild("Loot")
     local Flip = LootUI:WaitForChild("Frame"):WaitForChild("Flip")
     local Replay = LocalPlayer.PlayerGui:WaitForChild("ReadyScreen"):WaitForChild("Frame"):WaitForChild("Replay")
-    
+
     -- Destroy fx --
     Effects.ChildAdded:Connect(function(Child)
         if Child.Name ~= "DomainSphere" then
@@ -107,28 +123,20 @@ if not getgenv().StingrayLoaded then
     Destructibles.ChildAdded:Connect(function(Child)
         game:GetService("Debris"):AddItem(Child, 0)
     end)
-    
+
     -- Uh, ignore this spaghetti way of determining screen center --
     local MouseTarget = Instance.new("Frame", LocalPlayer.PlayerGui:FindFirstChildWhichIsA("ScreenGui"))
     MouseTarget.Size = UDim2.new(0, 0, 0, 0)
     MouseTarget.Position = UDim2.new(0.5, 0, 0.5, 0)
     MouseTarget.AnchorPoint = Vector2.new(0.5, 0.5)
     local X, Y = MouseTarget.AbsolutePosition.X, MouseTarget.AbsolutePosition.Y
-    
+
     -- Funcs --
-    
-    local function Encode(data) -- Pass data through url, safety encoding
-        local SafeStr = game:GetService("HttpService"):UrlEncode(data)
-        SafeStr = string.gsub(SafeStr, "+", "-")
-        SafeStr = string.gsub(SafeStr, "/", "_")
-        SafeStr = string.gsub(SafeStr, "=", "")
-        return SafeStr
-    end
-    
     local function Skill(Name)
-        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Combat"):WaitForChild("Skill"):FireServer(Name)
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Combat")
+            :WaitForChild("Skill"):FireServer(Name)
     end
-    
+
     local function OpenChest()
         for i, v in ipairs(Drops:GetChildren()) do
             if v:FindFirstChild("Collect") then
@@ -136,12 +144,7 @@ if not getgenv().StingrayLoaded then
             end
         end
     end
-    
-    local function Hit()
-        game:GetService("ReplicatedStorage").Remotes.Server.Combat:FindFirstChild("ApplyBlackFlashToNextHitbox"):FireServer()
-        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Combat"):WaitForChild("M2"):FireServer(2)
-    end
-    
+
     local function Click(Button)
         Button.AnchorPoint = Vector2.new(0.5, 0.5)
         Button.Size = UDim2.new(50, 0, 50, 0)
@@ -159,59 +162,50 @@ if not getgenv().StingrayLoaded then
         VIM:SendMouseButtonEvent(X, Y, 0, false, game, 0)
         task.wait()
     end
-    
+
+    local function InitTP()
+        --Root.CFrame = Spawns.BossSpawn.CFrame + Vector3.new(0, 10, 0)
+        local InitialTween = game:GetService("TweenService"):Create(Root, TweenInfo.new(1), {CFrame = Spawns.BossSpawn.CFrame + Vector3.new(0, 10, 0)})
+        InitialTween:Play()
+        InitialTween.Completed:Wait()
+        task.wait()
+    end
+
+    local function Domain(Name)
+        Skill("Domain Expansion: " .. Name)
+        task.wait()
+    end
+
     -- Farm start --
-    local ScriptLoading = tostring(math.floor((tick() - StartTime) * 10) / 10)
-    local InitialTween = game:GetService("TweenService"):Create(Root, TweenInfo.new(1), {CFrame = Spawns.BossSpawn.CFrame + Vector3.new(0, 10, 0)})
-    InitialTween:Play()
-    InitialTween.Completed:Wait()
-    repeat task.wait() until Mobs:FindFirstChildWhichIsA("Model")
+    repeat InitTP() until Mobs:FindFirstChildWhichIsA("Model")
     local Boss = Mobs:FindFirstChildWhichIsA("Model").Name
-    local Config
     game:GetService("ReplicatedStorage").Remotes.Client.GetClosestTarget.OnClientInvoke = function()
         return Mobs[Boss].Humanoid
     end
-    task.spawn(function()
-        local s, e = pcall(function()
-            writefile("Stingray/JJI_LastBoss.txt", Boss)
-        end)
-    end)
-    print("Aim hooked to "..Boss)
-    
+
+    Skill("Infinity: Mugen")
+
     -- Use boosts --
     local LotusActive = LocalPlayer.ReplicatedData.chestOverride
     local CatActive = LocalPlayer.ReplicatedData.luckBoost
-    local LotusValue, CatValue = 0, 0
-    task.spawn(function()
-        for _, Item in pairs(getgenv().LuckBoosts) do
-            task.wait()
-            if table.find(Loti, Item) and LotusActive.Value == 0 then
-                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Data"):WaitForChild("EquipItem"):InvokeServer(Item)
-                print(Item.." used")
-            end
-            task.wait(0.5)
-            if LotusActive.Value == 0 then
-                if (not table.find(Cats, Item)) or CatActive.duration.Value == 0 then
-                    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Data"):WaitForChild("EquipItem"):InvokeServer(Item)
-                    print(Item.." used")
-                end
+    for _, Item in pairs(getgenv().LuckBoosts) do
+        task.wait()
+        if table.find(Loti, Item) and LotusActive.Value == 0 then
+            print(Item.." used:", game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Data"):WaitForChild("EquipItem"):InvokeServer(Item))
+        end
+        task.wait(0.5)
+        if LotusActive.Value == 0 then
+            if (not table.find(Cats, Item)) or LocalPlayer.ReplicatedData.luckBoost.duration.Value == 0 then
+                print(Item.." used:", game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Data"):WaitForChild("EquipItem"):InvokeServer(Item))
             end
         end
-        LotusValue = LotusActive.Value
-        CatValue = CatActive.amount.Value
-    end)
-    
+    end
+
     -- Skill spam --
-    task.wait(1)
-    local Limiter = 0
-    local IsRunner = table.find(Runners, Boss)
-    if getgenv().InstantKill ~= "ON" then
+    task.wait(0.5)
+    if InstantKill ~= "ON" then
         repeat
-            if IsRunner then
-                Skill("Domain Expansion: Unlimited Void")
-            else
-                Skill("Incomplete Domain")
-            end
+            Skill("Incomplete Domain")
             task.wait(3)
         until Effects:FindFirstChild("DomainSphere")
         task.spawn(function()
@@ -219,39 +213,26 @@ if not getgenv().StingrayLoaded then
                 if Mobs[Boss].Humanoid.Health ~= Mobs[Boss].Humanoid.MaxHealth then
                     Mobs[Boss].Humanoid.Health = 0
                 end
-                if identifyexecutor() ~= "Solara" then
-                    Skill("Volcano: Ember Insects")
-                else
-                    Hit(Mobs:FindFirstChild(Boss).Humanoid)
-                end
-                Limiter = Limiter + 1
-                if Limiter % 240 == 0 then
-                    task.wait(1) -- Spam of ember insects can cause high pings, may cause disconnections and crashes
-                end
+                Skill("Volcano: Ember Insects")
                 task.wait()
             end
         end)
     end
-    
-    repeat task.wait() until Drops:FindFirstChild("Chest") -- Could have used WaitForChild here, but I felt it feels cursed not assigning WaitForChild to a variable, then I don't want an unused variable...
+
+    repeat task.wait() until Drops:FindFirstChild("Chest")
     local Items = "| "
     game:GetService("ReplicatedStorage").Remotes.Client.Notify.OnClientEvent:Connect(function(Message)
-        local Item = string.match(Message, '">(.-)')
-        if not (string.find(Item, "Stat Point") or string.find(Item, "Level")) then
-            if table.find(Highlight, Item) then
-                Item = "**"..Item.."**"
-            end
-            Items = Items .. Item .. " | "
+        local Item = string.match(Message, '">(.-)<')
+        if table.find(Highlight, Item) then
+            Item = "**"..Item.."**"
         end
+        Items = Items .. Item .. " | "
     end)
-    
+
     -- Overwrite chest collection function --
-    local ChestsCollected = 0
     local s, e = pcall(function()
-        game:GetService("ReplicatedStorage").Remotes.Client.CollectChest.OnClientInvoke = function(Chest)
-            if Chest then
-                ChestsCollected = ChestsCollected + 1
-            end
+        game:GetService("ReplicatedStorage").Remotes.Client.CollectChest.OnClientInvoke = function()
+            print("Chest Collected")
             return {}
         end
     end)
@@ -265,15 +246,15 @@ if not getgenv().StingrayLoaded then
             task.wait()
         end
     end)
-    
+
     repeat task.wait() until not (Drops:FindFirstChild("Chest") or LootUI.Enabled)
-    
+
     -- Send webhook message --
     pcall(function()
-        if getgenv().Webhook then
+        if true then
             task.wait(1)
             local embed = {
-                ["title"] = LocalPlayer.Name .. " has defeated " .. Boss .. " in " .. tostring(math.floor((tick() - StartTime) * 10) / 10) .. " seconds",
+                ["title"] = Vuong .. " has defeated " .. Boss .. " in " .. tostring(math.floor((tick() - StartTime) * 10) / 10) .. " seconds",
                 ['description'] = "Collected Items: " .. Items,
                 ["color"] = tonumber(000000)
             }
@@ -285,9 +266,13 @@ if not getgenv().StingrayLoaded then
             })
         end
     end)
-    
+
     -- Click replay --
     task.wait()
+    pcall(function()
+        writefile("JJI_LastBoss.txt", Boss)
+    end)
+    task.wait(1)
     for i = 1, 10, 1 do
         Click(Replay)
         task.wait(1)
